6c14aea11c344322a3f911107d1703afc51d8512
Vladimír Vondruš <mosra@centrum.cz>
2022-02-13 13:39:21 +0100

    build-status: good riddance, Travis CI.
    You were awful. You won't be missed.

--- content/build-status.js
+++ content/build-status.js
@@ -37,111 +37,6 @@ function timeDiff(before, now) {
         return "now";
 }
 
-function fetchTravisJobStatus(latestJobs) {
-    var req = window.XDomainRequest ? new XDomainRequest() : new XMLHttpRequest();
-    if(!req) return;
-
-    req.open("GET", 'https://api.travis-ci.com/jobs?ids[]=' + latestJobs.join('&ids[]='), true);
-    req.setRequestHeader("Accept", "application/vnd.travis-ci.2.1+json");
-    req.responseType = 'json';
-    req.onreadystatechange = function() {
-        if(req.readyState != 4) return;
-
-        //console.log(req.response);
-
-        var now = new Date(Date.now());
-        var jobs = req.response['jobs'];
-        for(var i = 0; i != jobs.length; ++i) {
-            var env = jobs[i]['config']['env'];
-            if(!env) {
-                console.log("Travis job doesn't have any environment, skipping:", jobs[i]['repository_slug']);
-                continue;
-            }
-            var match = env.match(travisJobIdRe);
-            if(!match) continue;
-
-            /* ID is combined repository name (w/o author) and the job ID from
-               environment */
-            var repo = jobs[i]['repository_slug'];
-            var id = repo.substr(repo.indexOf('/') + 1) + "-" + match[1];
-            var elem = document.getElementById(id);
-            if(!elem) {
-                console.log('Unknown Travis job ID', id);
-                continue;
-            }
-
-            var type;
-            var status;
-            var ageField;
-            if(jobs[i]['state'] == 'passed') {
-                /* Travis CI is useless right now, so it doesn't make sense to
-                   mark the increasingly stale builds green. */
-                type = 'm-dim';
-                status = '✔';
-                ageField = 'finished_at';
-            } else if(jobs[i]['state'] == 'started') {
-                type = 'm-warning';
-                status = '↺';
-                ageField = 'started_at';
-            } else if(jobs[i]['state'] == 'canceled') {
-                type = 'm-dim';
-                status = '∅';
-                ageField = 'finished_at';
-            } else if(jobs[i]['state'] == 'received' ||
-                      jobs[i]['state'] == 'created' ||
-                      jobs[i]['state'] == 'queued') {
-                type = 'm-info';
-                status = '…';
-                ageField = '';
-            } else if(jobs[i]['state'] == 'errored' ||
-                      jobs[i]['state'] == 'failed') {
-                type = 'm-danger';
-                status = '✘';
-                ageField = 'finished_at';
-            } else {
-                type = 'm-default';
-                status = jobs[i]['state'];
-                ageField = 'started_at';
-            }
-
-            var age;
-            var title;
-            if(ageField) {
-                age = timeDiff(new Date(Date.parse(jobs[i][ageField])), now);
-                title = jobs[i]['state'] + ' @ ' + jobs[i][ageField];
-            } else {
-                age = '&nbsp;'; /* To avoid layout jumps */
-                title = jobs[i]['state'];
-            }
-
-            /* Update the field only if it's not already provided by some other
-               provider */
-            if(!elem.className) {
-                elem.innerHTML = '<a href="https://travis-ci.com/' + repo + '/jobs/' + jobs[i]['id'] + '" title="' + title + '">' + status + '<br /><span class="m-text m-small">' + age + '</span></a>';
-                elem.className = type;
-            }
-        }
-    };
-    req.send();
-}
-
-function fetchLatestTravisJobs(project, branch) {
-    var req = window.XDomainRequest ? new XDomainRequest() : new XMLHttpRequest();
-    if(!req) return;
-
-    req.open("GET", 'https://api.travis-ci.com/repos/' + project + '/branches/' + branch, true);
-    req.setRequestHeader("Accept", "application/vnd.travis-ci.2.1+json");
-    req.responseType = 'json';
-    req.onreadystatechange = function() {
-        if(req.readyState != 4 || req.status != 200) return;
-
-        /* Originally I was putting all these IDs into a giant array and
-           fetching it just once. But I don't care anymore. */
-        fetchTravisJobStatus(req.response['branch']['job_ids']);
-    };
-    req.send();
-}
-
 /* https://circleci.com/docs/api/#recent-builds-for-a-single-project */
 function fetchLatestCircleCiJobs(project, branch) {
     var req = window.XDomainRequest ? new XDomainRequest() : new XMLHttpRequest();
@@ -335,7 +230,6 @@ function fetchLatestCodecovJobs(project, branch) {
 
 function fetch() {
     for(var i = 0; i != projects.length; ++i) {
-//         fetchLatestTravisJobs(projects[i][0], projects[i][1]);
         fetchLatestCircleCiJobs(projects[i][0], projects[i][1]);
         /* These are not on AppVeyor */
         if(projects[i][0].indexOf('flextgl') === -1 &&
--- content/build-status.rst
+++ content/build-status.rst
@@ -39,14 +39,6 @@ Show builds for:
 -   `master branch <{filename}/build-status.rst>`_
 -   `next branch <{filename}/build-status.rst?mosra/corrade=next&mosra/magnum=next&mosra/magnum-plugins=next&mosra/magnum-extras=next&mosra/magnum-integration=next&mosra/magnum-bindings=next&mosra/magnum-examples=next&mosra/magnum-examples=ports-next&mosra/magnum-bootstrap=next&mosra/flextgl=next&mosra/magnum-singles=next&mosra/homebrew-magnum=next>`_
 
-.. block-warning:: Travis CI woes
-
-    Travis CI recently switched all OSS projects to `trial plans with a one-time credit allowance <https://blog.travis-ci.com/2020-11-02-travis-ci-new-billing>`_,
-    practically preventing everyone from using their services. Affected builds,
-    which will get increasingly stale, are marked with a :label-flat-dim:`dim color`
-    and migration to other services is underway. Stability of the ``master``
-    branch is temporarily not guaranteed on iOS and Android platforms.
-
 `Main repositories`_
 ====================
 
