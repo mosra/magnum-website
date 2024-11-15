mosra/magnum-website

commit f394c13def1a77711fb3671176e77fef8f1ab570

Upgrade to Pelican 4.0 properly.

pelicanconf.py
@@ -178,8 +178,14 @@ if not shutil.which('latex'):
     M_MATH_RENDER_AS_CODE = True
 
 DIRECT_TEMPLATES = ['archives']
-PAGINATED_DIRECT_TEMPLATES = ['archives']
+PAGINATED_TEMPLATES = {'archives': None, 'tag': None, 'category': None, 'author': None}
 
 SLUGIFY_SOURCE = 'basename'
 PATH_METADATA = '(blog/)?(?P<slug>.+).rst'
-SLUG_REGEX_SUBSTITUTIONS = [('C\+\+', 'cpp')]
+SLUG_REGEX_SUBSTITUTIONS = [
+        (r'[^\w\s-]', ''),  # remove non-alphabetical/whitespace/'-' chars
+        (r'(?u)\A\s*', ''),  # strip leading whitespace
+        (r'(?u)\s*\Z', ''),  # strip trailing whitespace
+        (r'[-\s]+', '-'),  # reduce multiple whitespace or '-' to single '-'
+        (r'C\+\+', 'cpp'),
+    ]

commit 212a40e1fecf25d25d8488359b9c63ac072f4668

Fix C++ -> cpp slug replacement.
Broken since the upgrade in f394c13def1a77711fb3671176e77fef8f1ab570.

pelicanconf.py
@@ -240,9 +240,12 @@ PAGINATED_TEMPLATES = {'archives': None, 'tag': None, 'category': None, 'author'
 SLUGIFY_SOURCE = 'basename'
 PATH_METADATA = '(blog/)?(?P<slug>.+).rst'
 SLUG_REGEX_SUBSTITUTIONS = [
-        (r'[^\w\s-]', ''),  # remove non-alphabetical/whitespace/'-' chars
-        (r'(?u)\A\s*', ''),  # strip leading whitespace
-        (r'(?u)\s*\Z', ''),  # strip trailing whitespace
-        (r'[-\s]+', '-'),  # reduce multiple whitespace or '-' to single '-'
-        (r'C\+\+', 'cpp'),
-    ]
+    # Has to be first so the ++ isn't removed by the time it reaches this
+    (r'C\+\+', 'cpp'),
+
+    # Pelican defaults
+    (r'[^\w\s-]', ''),      # remove non-alphabetical/whitespace/'-' chars
+    (r'(?u)\A\s*', ''),     # strip leading whitespace
+    (r'(?u)\s*\Z', ''),     # strip trailing whitespace
+    (r'[-\s]+', '-'),       # reduce multiple whitespace or '-' to single '-'
+]